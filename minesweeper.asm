7996d3ebc046a893162315a8b3e7d2359eb411e2
Tepoys <ytTepois@gmail.com>
2026-01-16 22:02:34 -0500

    Added coloring to printing.

--- minesweeper.asm
+++ minesweeper.asm
@@ -13,17 +13,32 @@ DEFAULT ABS
 %define FOUR_UNREVEALED 4
 %define FIVE_UNREVEALED 5
 %define SIX_UNREVEALED 6
-%define SEVEN_UNREVEALD 7
+%define SEVEN_UNREVEALED 7
 %define EIGHT_UNREVEALED 8
-%define ZERO_UNREVEALED 0
+; %define ZERO_REVEALED 10
+; for itterating over 0 chunks to auto display numbers
+%define ZERO_REVEALED_ITTERATED 32
 %define ONE_REVEALED 11
 %define TWO_REVEALED 12
 %define THREE_REVEALED 13
 %define FOUR_REVEALED 14
 %define FIVE_REVEALED 15
 %define SIX_REVEALED 16
-%define SEVEN_REVEALD 17
+%define SEVEN_REVEALED 17
 %define EIGHT_REVEALED 18
+
+%define ZERO_FLAGGED 20
+%define ONE_FLAGGED 21
+%define TWO_FLAGGED 22
+%define THREE_FLAGGED 23
+%define FOUR_FLAGGED 24
+%define FIVE_FLAGGED 25
+%define SIX_FLAGGED 26
+%define SEVEN_FLAGGED 27
+%define EIGHT_FLAGGED 28
+
+%define BOMB_REVEALED 30
+%define REAL_BOMB_FLAG 29
 
 ; rdi - minesweeper x length; rsi - minesweeper y length; rdx - minesweeper mine count
 global minesweeper
@@ -41,8 +56,6 @@ extern free
 
 extern getRand
 
-
-
 section .data
   mallocFailedMsg db "Malloc returned null pointer (malloc failure).", 10, "Program exit", 10, 0
   mine db "X ", 0
@@ -50,11 +63,46 @@ section .data
   printSquareNumber db "%u ", 0
   newLine db 10, 0
   debugMinelayingAttempt db "Attempting to lay mine at (%u,%u)", 10, 0
+  background db 0x1B, "[48;5;252m", 0
+  hidden db 0x1B, "[48;5;236m", 0x1B, "[38;5;250m", 0
+  hiddenText db "[O]", 0
+  cursorHighlight db 0x1B, "[48;5;24m", 0x1B, "[38;5;231m", 0
+  flag db 0x1B, "[48;5;236m", 0x1B, "[38;5;196m", 0x1B, "[1m", 0
+  flagText db "[F]", 0
+  bomb db 0x1B, "[48;5;160m", 0x1B, "[38;5;16m", 0x1B, "[1m", 0
+  bombText db "[X]", 0
+  zero db 0x1B, "[48;5;252m", 0x1B, "[38;5;240m", 0
+  zeroText db "[0]", 0
+  one db 0x1B, "[38;5;33m", 0
+  oneText db "[1]", 0
+  two db 0x1B, "[38;5;34m", 0
+  twoText db "[2]", 0
+  three db 0x1B, "[38;5;196m", 0
+  threeText db "[3]", 0
+  four db 0x1B, "[38;5;99m", 0
+  fourText db "[4]", 0
+  five db 0x1B, "[38;5;124m", 0
+  fiveText db "[5]", 0
+  six db 0x1B, "[38;5;37m", 0
+  sixText db "[6]", 0
+  seven db 0x1B, "[38;5;238m", 0
+  sevenText db "[7]", 0
+  eight db 0x1B, "[38;5;250m", 0
+  eightText db "[8]", 0
+  reset db 0x1B, "[0m", 0
+  space db "[ ]", 0
+
 
 section .bss
   map resq 1
+  ; dword since they can go negative (and default int is 32 bit)
+  mineCount resd 1
+  flagCount resd 1
+
   y resq 1
   x resq 1
+  currentX resq 1
+  currentY resq 1
   allocState resq 1
 
 section .text
@@ -99,6 +147,13 @@ minesweeper:
   mov rsi, qword[x]
   mov rdx, qword[y]
   call printMinefieldSmart
+
+  mov rdi, qword[map]
+  mov rsi, qword[x]
+  mov rdx, qword[y]
+  mov qword[currentX], 5
+  mov qword[currentY], 5
+  call printMinefieldWithColor
 
   add rsp, 32
   pop rbp
@@ -184,7 +239,7 @@ generateMines:
   mov rdi, 0
   mov rsi, qword[rbp-24]
   call getRand
-  
+
 ; .debugPrint:
 ;   ; debug
 ;   push rax
@@ -580,4 +635,218 @@ printMinefieldSmart:
   pop rbp
   ret
 
+; rdi - minefield; rsi - x length; rdx - y length; no returns
+; assumes player position is stored in currentX and currentY
+; prints minefield to screen, adds color and also displays
+printMinefieldWithColor:
+  ; r15 - y pointer; r14 - y length left itterator
+  ; r13 - x pointer; r12 - x length left itterator
+  push rbp
+  push r15
+  push r14
+  push r13
+  push r12
+  mov rbp, rsp
+  sub rsp, 32
 
+  ; original minefield pointer in r15
+  mov r15, rdi
+  ; total y length in r14
+  mov r14,  rdx
+  ; y itterator
+  mov qword[rbp - 24], 0
+
+  ; total x length
+  mov qword[rbp-8], rsi
+
+.printLoopY:
+  ; set up x pointer
+  mov r13, qword[r15]
+  ; set up x length
+  mov r12, qword[rbp-8]
+  ; set up x iterator
+  mov qword[rbp - 16], 0
+
+.printLoopX:
+  ; check cursor position
+  mov rax, qword[currentX]
+  cmp rax, qword[rbp-16]
+  jne .inconsistentXPosition
+
+  mov rax, qword[currentY]
+  cmp rax, qword[rbp-24]
+  jne .inconsistentYPosition
+  jmp .overrideBackground
+.overrideBackground:
+  mov rax, 0
+  mov rdi, reset
+  call printf
+  mov rax, 0
+  mov rdi, cursorHighlight
+  call printf
+  ; TODO move to bellow
+  
+.inconsistentYPosition:
+.inconsistentXPosition:
+  cmp byte[r13], 10
+  jl .unrevealed
+  jmp .revealed
+
+.unrevealed:
+; potential cheat feature
+  xor rax, rax
+  mov rdi, hidden
+  call printf
+
+  ; print O
+  mov rdi, hiddenText
+  jmp .printSquare
+
+.revealed:
+  cmp byte[r13], ZERO_FLAGGED
+  jnge .notFlagged
+  cmp byte[r13], REAL_BOMB_FLAG
+  jle .hasFlag
+  jmp .notFlagged
+
+.notFlagged:
+  cmp byte[r13], ZERO_REVEALED_ITTERATED
+  je .hasZero
+  cmp byte[r13], ONE_REVEALED
+  je .hasOne
+  cmp byte[r13], TWO_REVEALED
+  je .hasTwo
+  cmp byte[r13], THREE_REVEALED
+  je .hasThree
+  cmp byte[r13], FOUR_REVEALED
+  je .hasFour
+  cmp byte[r13], FIVE_REVEALED
+  je .hasFive
+  cmp byte[r13], SIX_REVEALED
+  je .hasSix
+  cmp byte[r13], SEVEN_REVEALED
+  je .hasSeven
+  cmp byte[r13], EIGHT_REVEALED
+  je .hasEight
+
+.hasFlag:
+  mov rax, 0
+  mov rdi, flag
+  call printf
+
+  mov rdi, flagText
+  jmp .printSquare
+
+.hasZero:
+  mov rax, 0
+  mov rdi, zero
+  call printf
+
+  mov rdi, zeroText
+  jmp .printSquare
+
+.hasOne:
+  mov rax, 0
+  mov rdi, one
+  call printf
+
+  mov rdi, oneText
+  jmp .printSquare
+
+.hasTwo:
+  mov rax, 0
+  mov rdi, two
+  call printf
+
+  mov rdi, twoText
+  jmp .printSquare
+
+.hasThree:
+  mov rax, 0
+  mov rdi, three
+  call printf
+
+  mov rdi, threeText
+  jmp .printSquare
+
+.hasFour:
+  mov rax, 0
+  mov rdi, four
+  call printf
+
+  mov rdi, fourText
+  jmp .printSquare
+
+.hasFive:
+  mov rax, 0
+  mov rdi, five
+  call printf
+
+  mov rdi, fiveText
+  jmp .printSquare
+
+.hasSix:
+  mov rax, 0
+  mov rdi, six
+  call printf
+
+  mov rdi, sixText
+  jmp .printSquare
+
+.hasSeven:
+  mov rax, 0
+  mov rdi, seven
+  call printf
+
+  mov rdi, sevenText
+  jmp .printSquare
+
+.hasEight:
+  mov rax, 0
+  mov rdi, eight
+  call printf
+
+  mov rdi, eightText
+  jmp .printSquare
+
+  ; print the current square
+.printSquare:
+  xor rax, rax
+  call printf
+
+  xor rax, rax
+  mov rdi, background
+  ;call printf
+
+  xor rax, rax
+  mov rdi, space
+  ;call printf
+
+  xor rax, rax
+  mov rdi, reset
+  call printf
+
+  ; itterate to the next square
+  inc r13 
+  inc qword[rbp - 16]
+  dec r12
+  jnz .printLoopX
+
+  ; print new line
+  xor rax, rax
+  mov rdi, newLine
+  call printf
+
+  ; end of x loop, itterate over y
+  add r15, 8
+  inc qword[rbp  - 24]
+  dec r14
+  jnz .printLoopY
+  
+  add rsp, 32
+  pop r12
+  pop r13
+  pop r14
+  pop r15
+  pop rbp
+  ret
